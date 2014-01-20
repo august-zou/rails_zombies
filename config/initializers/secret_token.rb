a4621e19560613804dad8c9be056bbc6d160702a
august-zou <zjzouxu@gmail.com>
2014-01-20 16:22:09 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsZombies::Application.config.secret_key_base = '469f8d2281178ca50bf2fb3e5ecc734aca6c5296b8c66c33585f97b09ad6e3990b211a8933aa4f287ea4246bf33f516afa50b4883b4ec1824da4974d4d1e1bd4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsZombies::Application.config.session_store :cookie_store, key: '_rails_zombies_session'
